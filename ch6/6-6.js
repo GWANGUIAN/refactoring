e188ec9bb9cc8e0e6b1472efbe054aec224ca6f0
GWANGUIAN <bbaa3218@gmail.com>
2022-07-05 21:46:48 +0900

    refactor: 6-6, 6-8, 6-9, 6-10.js

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,4 +1,19 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #lastName;
+  #firstName;
+  constructor(firstName, lastName) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+  }
+  get firstName() {
+    return this.#firstName;
+  }
+  get lastName() {
+    return this.#lastName;
+  }
+}
+
+let defaultOwner = new Person('마틴', '파울러');
 
 export function getDefaultOwner() {
   return defaultOwner;
--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,5 +1,26 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter((r) => !range.contains(r.temp));
+}
+
+export class NumberRange {
+  #max;
+  #min;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+
+  get min() {
+    return this._min;
+  }
+
+  get max() {
+    return this._max;
+  }
+
+  contains(number) {
+    return number >= this.#min && number <= this.#max;
+  }
 }
 
 const station = {
@@ -12,13 +33,10 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+
+const operationPlan = new NumberRange(51, 53);
 
 readingsOutsideRange(
   station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
+  operationPlan
 );
