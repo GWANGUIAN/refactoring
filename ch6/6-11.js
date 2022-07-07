d9e158137ed2b810b3fad8aedf54cf69292f6bb6
GWANGUIAN <bbaa3218@gmail.com>
2022-07-07 22:27:26 +0900

    refactor: 6-11

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,24 @@
 export function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePriceData (product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePriceData (product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  return {basePrice, discount, quantity};
+}
+
+function applyShipping (priceData, shippingMethod) {
   const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  priceData.basePrice > shippingMethod.discountThreshold
+    ? shippingMethod.discountedFee
+    : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
 
 // 사용 예:
