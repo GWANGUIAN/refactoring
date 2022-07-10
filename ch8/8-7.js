b499c81b84b7abb94d04f7427a4b49c61cedb44c
GWANGUIAN <bbaa3218@gmail.com>
2022-07-10 17:44:29 +0900

    refactor: ch8

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -1,10 +1,11 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+  return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+  function youngestAge () {
+    return Math.min(...people.map(person => person.age));
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  function totalSalary () {
+    return people.reduce((total, person) => total + person.salary, 0);
+  }
 }
