fc930459eaae71841030cd46e7e4cab7e4f878b2
GWANGUIAN <bbaa3218@gmail.com>
2022-07-15 23:28:24 +0900

    refactor: 10-1, 10-2, 10-3, 10-4-1.js

--- ch10/10-4-1.js
+++ ch10/10-4-1.js
@@ -1,32 +1,73 @@
+class Bird {
+  constructor(bird) {
+    Object.assign(this, bird);
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  get plumage() {
+    return 'unknown';
+  }
+
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return 'average';
+  }
+
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? 'scorched' : 'beautiful'
+  }
+
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
+
+const createBird = (bird) => {
+  switch (bird.type) {
+    case 'EuropeanSwallow':
+      return new EuropeanSwallow(bird);
+    case 'AfricanSwallow':
+      return new AfricanSwallow(bird);
+    case 'NorwegianBlueParrot':
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+}
+
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
-  let map1 = new Map(map);
-  return map1;
+  return new Map(
+    birds.map((b) => createBird(b))
+    .map((b) => [b.name, b.plumage])
+  );
 }
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
-}
-export function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
-  }
-}
-export function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
+  return new Map(
+    birds.map((b) => createBird(b))
+    .map((b) => [b.name, b.airSpeedVelocity])
+  );
 }
